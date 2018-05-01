9ce22387be3b34952eb665b4dd990a1b0670e65a
Kevin Cheung <kevinchikitcheung@gmail.com>
2018-05-01 12:43:24 -0500

    incomplete javascript-1

--- practice.js
+++ practice.js
@@ -5,28 +5,32 @@
 //greeting should return the string "Hello, "
 //plus the value of the name parameter.
 
-//Code here
+function greeting(param){
+  return "Hello, " + param;
+}//Code here
 
 //////////////////PROBLEM 2////////////////////
 
 //Rewrite the function greeting as a function expression.
 //Name it newGreeting.
 
-//Code Here
+var newGreeting = function(param){
+  return "Hello, " + param;
+}//Code Here
 
 //////////////////PROBLEM 3////////////////////
 
 //Rewrite the function greeting as an arrow function.
 //Name it finalGreeting.
 
-//Code Here
+var finalGreeting = param => "Hello, " + param//Code Here
 
 //////////////////PROBLEM 4////////////////////
 
 //Create an array called groceries with the values
 //"apples", "milk", "eggs", "bread"
 
-//Code Here
+var groceries = ["apples", "milk", "eggs", "bread"];//Code Here
 
 //Write a function called doubleCheck that takes in an array
 //as a parameter.
@@ -34,7 +38,12 @@
 //If the array does not contain "chocolate", add "chocolate".
 //doubleCheck should return the array.
 
-//Code Here
+function doubleCheck(array){
+  if (array !=  "chocolate"){
+    array.push("chocolate");
+    }
+    return array;
+  }//Code Here
 
 //////////////////PROBLEM 5////////////////////
 
@@ -43,21 +52,28 @@
 //name (a string), color: (a string), age (a number),
 //and goodBoy (a boolean).
 
-//Code Here
+var dog = {
+  name: "Sebastian",
+  color: "brown",
+  age: 20,
+  goodBoy: true
+  }//Code Here
 
 //...access the dog's name from the object and assign it to a
 //variable called devMountainClassPet.
 
-//Code Here
+var devMountainClassPet = dog.name;//Code Here
 
 //Add a method to dog called bark.
 //The value of bark should be a function that returns the string "Woof woof".
 
-//Code Here
+dog.bark = function(){
+  return "Woof woof";
+}//Code Here
 
 //Store the result of invoking the bark method in a variable called ruff.
 
-//Code Here
+var ruff = dog.bark();//Code Here
 
 //////////////////PROBLEM 6////////////////////
 
@@ -70,7 +86,14 @@
 
 //Return mySum.
 
-//Code Here
+function looper(array){
+  var mySum = 0;
+  for (var i = 0; i < array.length; i++){
+    if (array[i] % 2 !== 0 || array[i] >= 100){
+      return array[i] + mySum;
+    }
+  }
+}//Code Here
 
 //////////////////PROBLEM 7////////////////////
 
@@ -83,12 +106,14 @@ function math(num1, num2, callback) {
 //Write a function called add that takes in two parameters and
 //returns the result of adding them together.
 
-//Code Here
+function add(num1, num2){
+  return num1 + num2;
+}//Code Here
 
 //Now invoke math, passing in the numbers 3 and 4, and your add function,
 //storing the result in the variable mathSum.
 
-//Code Here
+var mathSum = math(3, 4, add);//Code Here
 
 //////////////////PROBLEM 8////////////////////
 
@@ -103,7 +128,9 @@ function sampleCallbackTwo() {
   return "I am also a callback function";
 }
 
-//Code Here
+function invoker(callback){
+  return callback();
+}//Code Here
 
 //////////////////PROBLEM 9////////////////////
 
@@ -143,12 +170,16 @@ let pondScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
 //Create a function called outerFn which returns an anonymous
 //function which returns your name.
 
-//Code Here
+function outerFn(){
+  return function(){
+    return "Kevin Cheung";
+  }
+}//Code Here
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
-//Code Here
+var innerFn = outerFn();//Code Here
 
 //Now invoke innerFn and save the result to a variable called finalResult.
 
-//Code Here
+var finalResult = innerFn();//Code Here
